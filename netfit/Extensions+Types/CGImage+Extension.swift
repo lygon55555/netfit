605731e60391f82e189b8470072c70087f3bdec2
Yonghyun <lygon55555@gmail.com>
2020-07-14 09:30:01 +0900

    Extensions

---
+++ netfit/Extensions+Types/CGImage+Extension.swift
@@ -0,0 +1,15 @@
+//
+//  CGImage+Extension.swift
+//  netfit
+//
+//  Created by Yonghyun on 2020/07/13.
+//  Copyright © 2020 Yonghyun. All rights reserved.
+//
+
+import CoreGraphics
+
+extension CGImage {
+    var size: CGSize {
+        return CGSize(width: width, height: height)
+    }
+}
