67ff46efa15548a8075f4b4d9c101a1e36e055fc
Yonghyun <lygon55555@gmail.com>
2020-07-15 18:51:16 +0900

    ShadowView Util

---
+++ netfit/Utils/ShadowView.swift
@@ -0,0 +1,69 @@
+//
+//  ShadowView.swift
+//  netfit
+//
+//  Created by Yonghyun on 2020/07/14.
+//  Copyright © 2020 Yonghyun. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+@IBDesignable class ShadowView: UIView {
+
+    @IBInspectable var shadowColor: UIColor? {
+        get {
+            if let color = layer.shadowColor {
+                return UIColor(cgColor: color)
+            }
+            return nil
+        }
+        set {
+            if let color = newValue {
+                layer.shadowColor = color.cgColor
+            } else {
+                layer.shadowColor = nil
+            }
+        }
+    }
+
+    @IBInspectable var shadowOpacity: Float {
+        get {
+            return layer.shadowOpacity
+        }
+        set {
+            layer.shadowOpacity = newValue
+        }
+    }
+
+    @IBInspectable var shadowOffset: CGPoint {
+        get {
+            return CGPoint(x: layer.shadowOffset.width, y:layer.shadowOffset.height)
+        }
+        set {
+            layer.shadowOffset = CGSize(width: newValue.x, height: newValue.y)
+        }
+
+     }
+
+    @IBInspectable var shadowBlur: CGFloat {
+        get {
+            return layer.shadowRadius
+        }
+        set {
+            layer.shadowRadius = newValue / 2.0
+        }
+    }
+
+    @IBInspectable var shadowSpread: CGFloat = 0 {
+        didSet {
+            if shadowSpread == 0 {
+                layer.shadowPath = nil
+            } else {
+                let dx = -shadowSpread
+                let rect = bounds.insetBy(dx: dx, dy: dx)
+                layer.shadowPath = UIBezierPath(rect: rect).cgPath
+            }
+        }
+    }
+}
